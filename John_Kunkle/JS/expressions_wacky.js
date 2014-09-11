f9b465415820134ad075cc93d169149909db506e
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 22:53:29 -0400

    wacky
    data set I think its an order issue

--- John_Kunkle/JS/expressions_wacky.js
+++ John_Kunkle/JS/expressions_wacky.js
@@ -1,3 +1,14 @@
 /**
  * Created by MyVyrus on 9/10/14.
  */
+// John Kunkle 9/10/14
+
+var dota2Heros = ["axe","ancientApparition", "shadowFiend", "dazzle", "luna" ];
+var axe = 53;
+var ancientApparition = 48;
+var shadowFiend = 45;
+var dazzle = 54;
+var luna = 60;
+var hero = prompt ("choose a dota2 Hero");
+var hero2 = prompt ("choose another dota2 hero");
+alert("Your average win rate is "+hero+hero2/2+"%")
