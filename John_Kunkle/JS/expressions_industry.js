bdac31b605aba70df20c22894c6f55cf4a7216a1
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 22:20:09 -0400

    changes to expressions
    set equation

--- John_Kunkle/JS/expressions_industry.js
+++ John_Kunkle/JS/expressions_industry.js
@@ -2,6 +2,9 @@
  * Created by MyVyrus on 9/10/14.
  */
 // John Kunkle 9/10/14
-//gathering user information
+//bandwidth for running movies
 
+var vidQuality = prompt("What video quality do you with to run?")
+//
+alert("you need "+vidQuality/210+" to run that quality." )
 
