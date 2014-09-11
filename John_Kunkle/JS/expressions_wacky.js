b1ce57c9c356aac6cff53c772857b034d76dc699
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 23:25:44 -0400

    wacky
    almost?

--- John_Kunkle/JS/expressions_wacky.js
+++ John_Kunkle/JS/expressions_wacky.js
@@ -3,12 +3,13 @@
  */
 // John Kunkle 9/10/14
 
-var dota2Heros = ["axe","ancientApparition", "shadowFiend", "dazzle", "luna" ];
+var dota2Heros = ["axe"," ancientApparition", " shadowFiend", " dazzle", " luna" ];
 var axe = 53;
 var ancientApparition = 48;
 var shadowFiend = 45;
 var dazzle = 54;
 var luna = 60;
+alert("These are the heroes you have to choose from!: " + dota2Heros);
 var hero = prompt ("choose a dota2 Hero");
 var hero2 = prompt ("choose another dota2 hero");
-alert("Your average win rate is "+hero+hero2/2+"%")
+alert("Your average win rate is "+hero+hero2/2+"%");
