dd759f1311b5cb87183e1b944ce69a6c4b4cc246
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 21:18:26 -0400

    It works
    ‘’

--- John_Kunkle/JS/expressions_personal.js
+++ John_Kunkle/JS/expressions_personal.js
@@ -12,6 +12,6 @@ var exercises = prompt ("How many exercises are you going to do for each muscle?
 //
 var sets = prompt("How many sets of each exercise?");
 //
-var reps = prompt("How many are you going to do for each");
-
+var reps = prompt("How many reps are you going to do for each");
+//
 console.log(muscleGroups*exercises*sets*reps);
