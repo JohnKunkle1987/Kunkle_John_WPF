1541959d86d4d2d460d8cbf2974b3e2ba126866d
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 21:23:51 -0400

    change to personal
    added comments

--- John_Kunkle/JS/expressions_industry.js
+++ John_Kunkle/JS/expressions_industry.js
@@ -1,3 +1,4 @@
 /**
  * Created by MyVyrus on 9/10/14.
  */
+// John Kunkle
--- John_Kunkle/JS/expressions_personal.js
+++ John_Kunkle/JS/expressions_personal.js
@@ -7,11 +7,12 @@
 
 
 var muscleGroups = prompt("How many muscle groups are you going to workout?");
-//
+//assigning var to the answer of the prompt
 var exercises = prompt ("How many exercises are you going to do for each muscle?");
-//
+//assigning var to the answer of the prompt
 var sets = prompt("How many sets of each exercise?");
-//
+//more prompts for var
 var reps = prompt("How many reps are you going to do for each");
-//
+//assigning reps to the question of how many reps they plan on doing
 console.log(muscleGroups*exercises*sets*reps);
+//taking all the answers(var) and multiplying them
