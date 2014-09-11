f7984d46cadc3501414b224a71a7135cea8dadd4
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-10 21:11:37 -0400

    start of personal
    var has been set

---
+++ John_Kunkle/JS/expressions_personal.js
@@ -0,0 +1,15 @@
+/**
+ * Created by MyVyrus on 9/10/14.
+ */
+// John Kunkle 9/10/14 WPF
+
+//reps for chest+tri workout at the gym on thursday
+
+
+var muscleGroups = prompt("How many muscle groups are you going to workout?");
+//
+var exercises = prompt ("How many exercises are you going to do for each muscle?");
+//
+var sets = prompt("How many sets of each exercise?");
+//
+var reps = prompt("How many are you going to do for each");
