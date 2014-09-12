e10a3db18543cb7f7b94d57f7bf0eede0b1ca656
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-12 17:42:07 -0400

    work on wacky
    stated questions still no test or comments

--- John_Kunkle/JS/expressions_wacky.js
+++ John_Kunkle/JS/expressions_wacky.js
@@ -3,4 +3,7 @@
  */
 // John Kunkle 9/10/14
 
-var 
+var one = []
+one[0]= prompt("How many fingers do you have on your right hand?");
+var two = prompt("How many hands do you have?")
+alert(one[0]*two+" is the number of fingers you should have.")
