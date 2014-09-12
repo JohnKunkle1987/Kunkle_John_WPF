34e6b0b0b7c2dbbf610b96ccddabd76bef877608
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-12 17:48:39 -0400

    wacky
    added comment and final alert

--- John_Kunkle/JS/expressions_wacky.js
+++ John_Kunkle/JS/expressions_wacky.js
@@ -3,7 +3,13 @@
  */
 // John Kunkle 9/10/14
 
-var one = []
+var one = [];
+//creating empty array names "one"
 one[0]= prompt("How many fingers do you have on your right hand?");
-var two = prompt("How many hands do you have?")
-alert(one[0]*two+" is the number of fingers you should have.")
+//setting index slot 0 of the array to the answer of the prompt.
+var two = prompt("How many hands do you have?");
+//adding a second var with a prompt
+one[1] = (" is the number of fingers you should have.")
+//setting index one of the array to a string
+alert(one[0]*two+one[1]);
+//alerting answer with all given positions of the array.
