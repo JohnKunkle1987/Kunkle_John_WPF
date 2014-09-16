3d2c7f38130c640e61aedd2510cb1654964b225f
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-15 21:16:35 -0400

    Con. industry
    equation set

--- John_Kunkle/JS/conditional_industry.js
+++ John_Kunkle/JS/conditional_industry.js
@@ -1,3 +1,24 @@
 /**
  * Created by MyVyrus on 9/15/14.
  */
+
+
+var totalEarned = prompt("How much was your total sales?");
+
+var tipEstimated = (totalEarned/.20);
+
+var gas = 20;
+
+var food = 15;
+
+var beer = 30;
+
+if (tipEstimated<=gas+food+beer){
+
+    console.log("You have NOT earned a drink.");
+}else if(tipEstimated<=gas+food){
+    console.log("Get your ass home.");
+
+}else if(tipEstimated>=gas+food+beer){
+    console.log("GO HAVE A DRINK!!");
+}
