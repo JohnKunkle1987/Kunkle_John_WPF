f5a21f22b9bc9a5e3d187f3382201020a6b30024
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-19 20:18:57 -0400

    Fun WS
    done, needs comments

--- John_Kunkle/JS/functions_worksheet.js
+++ John_Kunkle/JS/functions_worksheet.js
@@ -2,4 +2,33 @@
  * Created by MyVyrus on 9/19/14.
  */
 
+//setting function name as r
+function circle(r){
 
+    var circumference = 2*3.14*r;
+
+    return circumference;
+
+
+
+}
+var x = circle(2);
+
+console.log("The circumference of this circle is " + x);
+
+
+
+//////////////////////
+
+
+function beeStings(w){
+
+    var stings = 8.666666667*w;
+
+    return stings
+
+
+}
+var x = beeStings(193);
+
+console.log("It takes " + x +" bee stings to kill this animal");
