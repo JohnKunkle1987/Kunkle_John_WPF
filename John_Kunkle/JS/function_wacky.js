ce5016ba23cf731f2e37f66652f096b99e2efa11
JohnKunkle1987 <Myvyrus@fullsail.edu>
2014-09-22 21:08:22 -0400

    wacky personal and industry done
    ‘’ ‘’ need comment on wacky

--- John_Kunkle/JS/function_wacky.js
+++ John_Kunkle/JS/function_wacky.js
@@ -4,13 +4,13 @@
 
 var chairs = 8;
 var tables = 10;
-
-
+//setting the var for chairs and tables
+//setting the function to chair total
 function chairTotal(a,b){
 
   var totalChairs = a*b;
   return totalChairs;
 }
-
+//logging the function as the var i set
 var chairNum = chairTotal(chairs,tables);
-console.log(("There are ")+ chairNum +" in the room");
+console.log(("There are ")+ chairNum +" chairs in the room");
